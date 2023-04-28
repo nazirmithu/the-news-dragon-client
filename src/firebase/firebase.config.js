d2664cbb561c6dc517f23c0d37e3ed76c8366c96
nazirmithu <nazirmithu22@gmail.com>
2023-04-29 01:00:41 +0600

    the-news-dragon-client-with-bootstrap-and- express

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC2s0m8DTG-1AKniMdh0BdkXBDNKR-dWsU",
+  authDomain: "the-news-dragon-12fae.firebaseapp.com",
+  projectId: "the-news-dragon-12fae",
+  storageBucket: "the-news-dragon-12fae.appspot.com",
+  messagingSenderId: "387474499773",
+  appId: "1:387474499773:web:846b9b67e71ccc84316fd4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
