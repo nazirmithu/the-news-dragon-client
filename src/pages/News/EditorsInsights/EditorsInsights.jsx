589356fba0cc31f54e954265fa7af689a356e5ff
nazirmithu <nazirmithu22@gmail.com>
2023-04-30 20:09:24 +0600

    all news page and single news page and category news page add and other page updateed

---
+++ src/pages/News/EditorsInsights/EditorsInsights.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
+import first from '../../../assets/1.png';
+import second from '../../../assets/2.png';
+import third from '../../../assets/3.png';
+
+const EditorsInsights = () => {
+    return (
+        <Row xs={1} md={2} lg={3} className="g-4 mt-4">
+            <Col>
+                <Card>
+                    <Card.Img variant="top" src={first} />
+                    <Card.Body>
+                        <Card.Title>21 The Most Stylish Wedding Guest Dresses For Spring</Card.Title>
+                        <Card.Text>
+
+                        </Card.Text>
+                        <p>Jan 4, 2022</p>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card>
+                    <Card.Img variant="top" src={second} />
+                    <Card.Body>
+                        <Card.Title>21 The Most Stylish Wedding Guest Dresses For Spring</Card.Title>
+                        <Card.Text>
+
+                        </Card.Text>
+                        <p>Jan 4, 2022</p>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card>
+                    <Card.Img variant="top" src={third} />
+                    <Card.Body>
+                        <Card.Title>21 The Most Stylish Wedding Guest Dresses For Spring</Card.Title>
+                        <Card.Text>
+
+                        </Card.Text>
+                        <p>Jan 4, 2022</p>
+                    </Card.Body>
+                </Card>
+            </Col>
+        </Row>
+    );
+};
+
+export default EditorsInsights;
--- src/pages/Shared/LeftNav/LeftNav.jsx
+++ src/pages/Shared/LeftNav/LeftNav.jsx
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { Card, Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import first from '../../../assets/1.png';
+import second from '../../../assets/2.png';
+import third from '../../../assets/3.png';
+
 
 const LeftNav = () => {
     const [categories, setCategories] = useState([]);
@@ -22,6 +27,55 @@ const LeftNav = () => {
                         <Link to={`/category/${category.id}`} className='text-decoration-none text-black'>{category.name}</Link>
                     </p>)
                 }
+            </div>
+            <div>
+                <Row xs={1} md={1} className="g-4 mt-4">
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={first} />
+                            <Card.Body>
+                                <Card.Title>Bayern Slams Authorities Over Flight Delay to Club World Cup</Card.Title>
+                                <Card.Text>
+                               
+                                </Card.Text>
+                                <div className='d-flex flex-inline gap-4'>
+                                <p>Sports</p>
+                                <p>Jan 4, 2022</p>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={second} />
+                            <Card.Body>
+                                <Card.Title>Bayern Slams Authorities Over Flight Delay to Club World Cup</Card.Title>
+                                <Card.Text>
+                               
+                                </Card.Text>
+                                <div className='d-flex flex-inline gap-4'>
+                                <p>Sports</p>
+                                <p>Jan 4, 2022</p>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                            <Card.Img variant="top" src={third} />
+                            <Card.Body>
+                                <Card.Title>Bayern Slams Authorities Over Flight Delay to Club World Cup</Card.Title>
+                                <Card.Text>
+                                
+                                </Card.Text>
+                                <div className='d-flex flex-inline gap-4'>
+                                <p>Sports</p>
+                                <p>Jan 4, 2022</p>
+                                </div>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
             </div>
         </div>
     );
